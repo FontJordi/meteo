15d3d31475513d9df686140cc274100edacbdebe
FontJordi <jordifontmiralles@gmail.com>
2024-01-09 22:38:31 +0100

    working script (ugly)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Meteo etl project
+This is a personal project made with the intentions to learn 
+
+notes: current etl working. have to work on:
+
+imports on scrpit.py
+upload terraform to github
+upload airflow dags
+dockerize everything
+code airflow connections and rds permissions to access bucket (maybe user too?) assign role to rds.
+parametrize uploads to s3 (country)
--- script/script.py
+++ script/script.py
@@ -1,12 +1,37 @@
 import requests
 import sys
-import time 
+import datetime
 import json
+import pandas as pd
+import time 
+import os
 
+print(str(datetime.datetime.now()))
 
-sys.path.insert(0, '/home/kiwichi/WEATHERAPI/packages/')
+def set_file_wd():
+    os.chdir(os.path.dirname(os.path.abspath(os.path.abspath(__file__))))
+    #return os.getcwd()
 
-from packages import mod
+def move_up_one_level():
+    
+    os.chdir(os.pardir)
+    #return os.getcwd()
+
+def move_down_one_level(dir):
+    os.chdir(os.getcwd() + "/" + dir)
+
+set_file_wd()
+move_up_one_level()
+move_down_one_level("packages")
+
+sys.path.insert(0, os.getcwd())
+
+import mod
+
+set_file_wd()
+move_up_one_level()
+
+print(mod.current())
 
 final_list = mod.intersection(mod.read_first_column_csv("/home/kiwichi/WEATHERAPI/DATA/municipis_catalans.csv"),mod.get_cities("ES"))
 
@@ -14,15 +39,31 @@ with open(mod.current() + "/keys/api.txt","r") as f:
     WEATHER_API_KEY = f.readline()
 
 if __name__ == "__main__":
-    country_code = "ES"
-    for city in final_list:
 
+    country_code = "ES"
+    columns = ['coord/lon', 'coord/lat', 'weather/id', 'weather/main',
+       'weather/description', 'weather/icon', 'base', 'main/temp',
+       'main/feels_like', 'main/temp_min', 'main/temp_max', 'main/pressure',
+       'main/humidity', 'main/sea_level', 'main/grnd_level', 'visibility',
+       'wind/speed', 'wind/deg', 'wind/gust', 'clouds/all', 'dt', 'sys/type',
+       'sys/id', 'sys/country', 'sys/sunrise', 'sys/sunset', 'timezone', 'id',
+       'name', 'cod']
+    
+    df = pd.DataFrame(columns=columns)
+    
+    for city in final_list:
 
         url = "https://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&appid={API_KEY}".format(city_name = city, country_code = "ES", API_KEY = WEATHER_API_KEY)
         res = requests.get(url)
         data = res.json()
         data_string = json.dumps(data,indent=2)
 
+        df2 = pd.DataFrame(data = [mod.bardfunc(data)["values"]], columns = mod.bardfunc(data)["columns"])
+        df = pd.concat([df,df2]).reset_index(drop=True)
+
         mod.upload_data(data_string,"meteobucketfirst",country_code,city)
-        print(data_string)
-        time.sleep(1.5)
+
+        time.sleep(4)
+
+    mod.upload_data(df.to_csv(None),"meteobucketfirst",country = "historical" ,city = str(datetime.datetime.now()) )
+    mod.upload_data(df.to_csv(None),"meteobucketfirst",country = "CAT" ,city = "all" )
