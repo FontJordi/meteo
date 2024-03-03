fb98f2891215934dac2e134585eb0c9bccb7e5f0
FontJordi <jordifontmiralles@gmail.com>
2024-03-03 22:50:25 +0100

    added descriptions on mod.py

--- packages/mod.py
+++ packages/mod.py
@@ -5,37 +5,65 @@ import boto3
 from botocore.exceptions import ClientError
 
 def get_parent_directory(file_path):
+    """
+    Returns the parent directory of the given file path.
+    """
     return os.path.dirname(file_path)
 
 def main_dir(main_script_path):
+    """
+    Returns the parent directory of the main script path.
+    """
     parent_directory = get_parent_directory(main_script_path)
     return parent_directory
 
 def flatten(xss):
+    """
+    Flattens a list of lists into a single list.
+    """
     return [x for xs in xss for x in xs]
 
 def find_csv_filenames(path_to_dir, suffix=".csv"):
+    """
+    Finds all filenames with the specified suffix (default is ".csv") in the given directory path.
+    """
     filenames = os.listdir(path_to_dir)
     return [filename for filename in filenames if filename.endswith(suffix)]
 
 def current():
+    """
+    Returns the current working directory.
+    """
     return os.getcwd()
 
 def get_cities(path, country):
+    """
+    Reads a JSON file containing city data from the specified path and returns a list of city names for the given country.
+    """
     with open(os.path.join(path, "DATA", "city.list.json"), "r") as f:
         data = json.load(f)
     cities = [element["name"] for element in data if element["country"] == country]
     return cities
 
 def read_first_column_csv(path):
+    """
+    Reads the first column of a CSV file and returns it as a list.
+    """
     with open(path, "r") as f:
         list2 = [row.split(",")[0] for row in f]
     return list2
 
 def intersection(lst1, lst2):
+    """
+    Returns the intersection of two lists.
+    """
     return [value for value in lst1 if value in lst2]
 
 def upload_data(data, key, bucket):
+    """
+    Uploads data to an AWS S3 bucket with the specified key.
+    Returns True if the upload is successful, False otherwise.
+    """
     s3_client = boto3.client('s3')
     try:
         response = s3_client.put_object(Body=data, Bucket=bucket, Key=key)
@@ -45,6 +73,10 @@ def upload_data(data, key, bucket):
     return True
 
 def bardfunc(data):
+    """
+    Transforms data into a dictionary containing columns and values, suitable for database insertion.
+    Returns a dictionary with keys 'columns' and 'values'.
+    """
     columns = []
     values = []
     for key, value in data.items():
